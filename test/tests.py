be6ca56892ded2c3a24ab5bbcf3926b9fd92bb3b
Rishab Dudhia <rishabdudhia@gmail.com>
2021-05-09 18:27:57 -0700

    Working on 3

--- test/tests.py
+++ test/tests.py
@@ -31,5 +31,5 @@ tests = [
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['Tick0::i', 'sound', 'state1', 'state0', 'Tick0::res']
+watch = ['Tick1::i', 'state1', 'Tick1::res']
 
